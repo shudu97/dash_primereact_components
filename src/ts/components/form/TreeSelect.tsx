1ae68f40b788ce77886728df2215fbe579014d78
Shuang Du <shuang.du@mail.mcgill.ca>
2023-09-04 20:59:15 -0400

    Add control button for tree selection

--- src/ts/components/form/TreeSelect.tsx
+++ src/ts/components/form/TreeSelect.tsx
@@ -1,46 +1,92 @@
-import React, { useState, useEffect } from 'react'
-import { TreeSelect as PrimeReactTreeSelect, TreeSelectSelectionKeysType } from 'primereact/treeselect'
-import { TreeNode } from 'primereact/treenode'
-import { Button } from 'primereact/button'
-
+import React, { useState, useEffect } from 'react';
+import { TreeSelect as PrimeReactTreeSelect, TreeSelectSelectionKeysType } from 'primereact/treeselect';
+import { TreeNode } from 'primereact/treenode';
+import { Button, ButtonProps } from 'primereact/button';
+import { IconType } from 'primereact/utils';
 
 type Props = {
-    setProps: Function; 
-    id?: string; 
-    options?: TreeNode[]; 
-    value?: null | string | TreeSelectSelectionKeysType | TreeSelectSelectionKeysType[]; 
-    selection_mode?
-    meta_key_selection?: boolean; 
-    filter?: boolean
+    setProps: Function;
+    id?: string;
+    options?: TreeNode[];
+    value?: null | string | TreeSelectSelectionKeysType | TreeSelectSelectionKeysType[];
+    selection_mode?;
+    meta_key_selection?: boolean;
+    filter?: boolean;
+    expandedKeys?: { [key: string]: boolean };
+    expand_icon?:IconType<ButtonProps>; 
+    collapse_icon?: IconType<ButtonProps>
 }
 
 const TreeSelect = (props: Props) => {
     const {
-        setProps, 
-        id, 
-        options, 
-        value, 
-        selection_mode, 
-        meta_key_selection, 
-        filter
-    } = props; 
+        setProps,
+        id,
+        options,
+        value,
+        selection_mode,
+        meta_key_selection,
+        filter,
+        expandedKeys: expandedKeysProp, 
+        expand_icon, 
+        collapse_icon
+    } = props;
 
     const [selectedNodeKey, setSelectedNodeKey] = useState(value);
+    const [expandedKeys, setExpandedKeys] = useState(expandedKeysProp || {});
 
-    // Synchronize internal state with props if `value` prop changes
     useEffect(() => {
         setSelectedNodeKey(value);
     }, [value]);
 
+    useEffect(() => {
+        setExpandedKeys(expandedKeysProp);
+    }, [expandedKeysProp]);
+
     const handleChange = (e) => {
         const newValue = e.value;
         setSelectedNodeKey(newValue);
-        
-        // Update the Dash component props
+
         if (setProps) {
             setProps({ value: newValue });
         }
     };
+
+    const handleToggle = (e) => {
+        if (setProps) {
+            setProps({ expandedKeys: e.value });
+        }
+    };
+
+    const expandAll = () => {
+        let _expandedKeys = {};
+
+        for (let node of options) {
+            expandNode(node, _expandedKeys);
+        }
+
+        setProps({ expandedKeys: _expandedKeys });
+    };
+
+    const collapseAll = () => {
+        setProps({ expandedKeys: {} });
+    };
+
+    const expandNode = (node, _expandedKeys) => {
+        if (node.children && node.children.length) {
+            _expandedKeys[node.key] = true;
+
+            for (let child of node.children) {
+                expandNode(child, _expandedKeys);
+            }
+        }
+    };
+
+    const headerTemplate = (
+        <div className="p-3 pb-0">
+            <Button type="button" icon={expand_icon} onClick={expandAll} className="w-2rem h-2rem mr-2 p-button-outlined" />
+            <Button type="button" icon={collapse_icon} onClick={collapseAll} className="w-2rem h-2rem p-button-outlined" />
+        </div>
+    );
 
     return (
         <PrimeReactTreeSelect 
@@ -51,15 +97,16 @@ const TreeSelect = (props: Props) => {
             selectionMode={selection_mode}
             metaKeySelection={meta_key_selection}
             filter={filter}
+            expandedKeys={expandedKeys}
+            onToggle={handleToggle}
+            panelHeaderTemplate={headerTemplate}
         />
-
     )
 }
 
 TreeSelect.defaultProps = {
-    meta_key_selection: false, 
+    meta_key_selection: false,
     selection_mode: 'single'
-}
+};
 
-export default TreeSelect
-
+export default TreeSelect;
